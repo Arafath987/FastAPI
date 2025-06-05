6954baaf8b9e1957ad6b81c66de55429f621f1f1
Arafath987 <94624401+Arafath987@users.noreply.github.com>
2025-06-05 16:14:27 +0530

    database . removed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request, status
-from .database import engine
+from database import engine
 from .models import Base
 from .routers import auth, todos, admin, users
 from fastapi.staticfiles import StaticFiles
